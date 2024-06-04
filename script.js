55969e906a18a2839aa903f263ab37dd1f58365e
Kashid05 <134701876+Kashid05@users.noreply.github.com>
2024-06-04 14:51:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+    setTimeout(()=>{
+        let sloganLine = document.getElementById('slogan');
+        sloganLine.innerHTML = "Capturing Moments of Life in Pixel";
+    },10000)    
+
+
+    setTimeout(()=>{
+        let changeBgColor = document.getElementById('about');
+        changeBgColor.style.backgroundColor = 'lightgreen';
+    },5500)
+    setTimeout(()=>{
+        let changeBgColor = document.getElementById('gallery');
+        changeBgColor.style.backgroundColor = 'lightblue';
+    },5500)
